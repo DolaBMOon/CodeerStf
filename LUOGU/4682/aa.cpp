9e969f04bf5e3e37bd11088215ff8defcd63f484
DOlaBMOon <dolabmoon@gmali.com>
2018-09-02 22:13:10 +0800

    puzzle added

---
+++ LUOGU/4682/aa.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<cassert>
+
+using namespace std;
+
+#define Whats(x) cout<<#x<<" is "<<x<<endl
+#define Divhe() cout<<">>>>>>>>>>>>>>>"<<endl
+#define Divshe() cout<<"<<<<<<<<<<<<<<<"<<endl
+#define Oops() cout<<"!!!!!!!!!!!!!!!"<<endl
+
+/*
+-<Unlimited Blade Works>-
+*/
+
+#define LL long long
+
+LL Rand()
+{
+	return (unsigned LL)new char;
+}
+
+int main()
+{
+	cout<<Rand();
+	return 0;
+}
