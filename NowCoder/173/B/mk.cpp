9a2f607b198e273653f44b3fa344ae397c916c8e
DOlaBMOon <dolabmoon@gmail.com>
2018-09-16 20:39:16 +0800

    nowcoderQAQ

---
+++ NowCoder/173/B/mk.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<cassert>
+
+using namespace std;
+
+#define Whats(x) cout<<#x<<" is "<<(x)<<endl
+#define DivHim() cout<<">>>>>>>>>>>>>>>"<<endl
+#define DivHer() cout<<"<<<<<<<<<<<<<<<"<<endl
+#define Oops() cout<<"!!!!!!!!!!!!!!!"<<endl
+
+template<typename T> bool GetMin(T &a,T b)
+{
+	return ((a<=b)?false:(a=b,true));
+}
+
+template<typename T> bool GetMax(T &a,T b)
+{
+	return ((a>=b)?false:(a=b,true));
+}
+
+/*
+	 -<Unlimited Blade Works>-
+ */
+
+int main()
+{
+	return 0;
+}
---
+++ NowCoder/173/B/t.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<cassert>
+
+using namespace std;
+
+#define Whats(x) cout<<#x<<" is "<<(x)<<endl
+#define DivHim() cout<<">>>>>>>>>>>>>>>"<<endl
+#define DivHer() cout<<"<<<<<<<<<<<<<<<"<<endl
+#define Oops() cout<<"!!!!!!!!!!!!!!!"<<endl
+
+template<typename T> bool GetMin(T &a,T b)
+{
+	return ((a<=b)?false:(a=b,true));
+}
+
+template<typename T> bool GetMax(T &a,T b)
+{
+	return ((a>=b)?false:(a=b,true));
+}
+
+/*
+	 -<Unlimited Blade Works>-
+ */
+
+int a[10]={0,1,2,3,4,5,6,7,8,9};
+
+int main()
+{
+	memcpy(a+1,a+2,sizeof(int)*3);
+	for(int i=1;i<10;++i)
+		cout<<a[i]<<endl;
+	return 0;
+}
