569866bc6e38fa271cc070c0f9c7c6e3fa2e74f3
DOlaBMOon <dolabmoon@gmail.com>
2018-09-18 06:59:06 +0800

    abcdefg

---
+++ AtCoder/AGC027/B/a.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<cassert>
+
+using namespace std;
+
+#define Whats(x) cout<<#x<<" is "<<(x)<<endl
+#define DivHim() cout<<">>>>>>>>>>>>>>>"<<endl
+#define DivHer() cout<<"<<<<<<<<<<<<<<<"<<endl
+#define Oops() cout<<"!!!!!!!!!!!!!!!"<<endl
+
+template<typename T> bool GetMin(T &a,T b)
+{
+	return ((a<=b)?false:(a=b,true));
+}
+
+template<typename T> bool GetMax(T &a,T b)
+{
+	return ((a>=b)?false:(a=b,true));
+}
+
+/*
+	 -<Unlimited Blade Works>-
+ */
+
+#define int long long
+
+const int N=2e5+10;
+
+int n,x,s[N];
+
+signed main()
+{
+	scanf("%lld%lld",&n,&x);
+	for(int i=1;i<=n;++i)
+	{
+	}
+	return 0;
+}
